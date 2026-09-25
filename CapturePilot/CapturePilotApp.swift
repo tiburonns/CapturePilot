a6dd49db4003bcf663a46378a97a8bc36c2c95c9
tiburonns <5pjvjmckn7@privaterelay.appleid.com>
2026-09-25 13:45:49 -0600

    Inject LUT library into app environment

--- CapturePilot/CapturePilotApp.swift
+++ CapturePilot/CapturePilotApp.swift
@@ -5,12 +5,14 @@ struct CapturePilotApp: App {
     @UIApplicationDelegateAdaptor(CapturePilotAppDelegate.self) private var appDelegate
     @StateObject private var settings = AppSettings()
     @StateObject private var hud = HUDLayoutStore()
+    @StateObject private var lutStore = LUTStore()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(settings)
                 .environmentObject(hud)
+                .environmentObject(lutStore)
         }
     }
 }
