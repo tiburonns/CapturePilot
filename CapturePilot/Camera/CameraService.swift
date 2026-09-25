7b67621a6758873d8763b34280e4cbd5a0d89148
tiburonns <5pjvjmckn7@privaterelay.appleid.com>
2026-09-24 19:23:37 -0600

    Use modern video rotation API

--- CapturePilot/Camera/CameraService.swift
+++ CapturePilot/Camera/CameraService.swift
@@ -134,8 +134,9 @@ final class CameraService: NSObject, ObservableObject {
             session.addOutput(videoOutput)
         }
 
-        if let connection = videoOutput.connection(with: .video), connection.isVideoOrientationSupported {
-            connection.videoOrientation = .portrait
+        if let connection = videoOutput.connection(with: .video),
+           connection.isVideoRotationAngleSupported(90) {
+            connection.videoRotationAngle = 90
         }
 
         DispatchQueue.main.async {
